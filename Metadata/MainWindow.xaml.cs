4a052480b2b3495b40c738f004ad5a36d915d88b
Sheviak <sheviak.k@gmail.com>
2019-02-11 12:04:27 +0200

    UI
    Подключены nuget пакеты.
    Создано окно и главное меню программы.

---
+++ Metadata/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Metadata
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
