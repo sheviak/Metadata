eda45c2cfffea587223f260b0687452440b264ef
Sheviak <sheviak.k@gmail.com>
2019-02-11 22:22:57 +0200

    Получение файлов
    Добавлено диалоговое окно выбора папки с файлами.
    Добавлена фильтрация по расширению.

--- Metadata/ViewModel/MainViewModel.cs
+++ Metadata/ViewModel/MainViewModel.cs
@@ -1,12 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Metadata.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        public static string PATH = ".\\default";
+        public ObservableCollection<string> Files { get; set; } = new ObservableCollection<string>();
+
+        public MainViewModel()
+        {
+            string dirName = ".\\default";
+            var files = Directory.GetFiles(dirName);
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        public ICommand OpenDir => new RelayCommand(() => GetAllFilesInFolder());
+        public ICommand CloseProgram => new RelayCommand(() => Environment.Exit(0));
+        public ICommand MinimizedProgram => new RelayCommand(() => {
+            var app = System.Windows.Application.Current.Windows[0];
+            app.WindowState = WindowState.Minimized;
+        });
+
+        public ICommand ExportFileInfo => new RelayCommand(() => System.Windows.MessageBox.Show("File exported"));
+        public ICommand CopyFileInfo => new RelayCommand(() => System.Windows.MessageBox.Show("Info copied"));
+
+
+        /// <summary>
+        /// Метод заполняющий коллекцию файлами
+        /// </summary>
+        public void GetAllFilesInFolder()
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                PATH = folderBrowser.SelectedPath;
+                var allowedExtentions = new List<string>(new[] { ".png", ".jpg", ".pdf" });
+                var files = Directory
+                    .GetFiles(PATH)
+                    .Where(f => allowedExtentions.Contains(Path.GetExtension(f)));
+
+                foreach (string s in files)
+                {
+                    Files.Add(s);
+                }
+            }
+        }
+
+
     }
 }
